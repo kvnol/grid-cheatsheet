d9e47b98fce56aa911a869d81e8add0883066978
Kevin Oliveira <me@kevinoliveira.com.br>
2017-03-14 22:48:01 -0300

    Create task to sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,19 @@
-gulp.task('default');
+'use strict';
+
+const gulp = require('gulp'),
+      sass = require('gulp-sass'),
+      clean = require('gulp-clean');
+
+gulp.task('sass', function() {
+  return gulp.src('./src/sass/main.sass')
+    .pipe(sass.sync({
+      outputStyle: 'compressed'
+    }).on('error', sass.logError))
+    .pipe(gulp.dest('./assets/css'));
+});
+
+gulp.task('sass:watch', function() {
+  gulp.watch('./src/sass/**/*.sass', ['sass']);
+});
+
+gulp.task('default', ['sass']);
